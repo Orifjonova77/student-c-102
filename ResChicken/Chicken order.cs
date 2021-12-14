c91aae539ab1ab914033234fc41b79eaf812e101
Orifjonova77 <94947335+Orifjonova77@users.noreply.github.com>
2021-12-14 11:41:31 +0500

    My first project commit

---
+++ ResChicken/Chicken order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResChicken
+{
+    class Chicken_order
+    {
+    private int quantity;
+
+        public Chicken_order(int quantity)
+        {
+            this.quantity = quantity;
+        }
+        public int GetQuantity()
+        {
+            return quantity;
+        }
+        public void CutUp()
+        {
+        }
+        public void Cook()
+        {
+        }
+    }
+}
---
+++ ResChicken/Egg order.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResChicken
+{
+    class Egg_order
+    {
+     private static Random rnd = new Random();
+        private int quantity;
+        private int quality = rnd.Next(101);
+        private static int qualityCounter = 0;
+        public Egg_order(int quantity)
+        {
+            this.quantity = quantity;
+            qualityCounter++;
+        }
+        public int GetQuantity()
+        {
+            return quantity;
+        }
+        public int? GetQuality()
+        {
+
+            if (qualityCounter % 2 == 1)
+            {
+                return quality;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public void Crack()
+        {
+            if (GetQuality() < 25)
+                throw new Exception("Качество:" + GetQuality() + " яйцо испорчено!");
+        }
+        public void DiscardShell()
+        {
+        }
+        public void Cook()
+        {
+        }
+    }
+}
